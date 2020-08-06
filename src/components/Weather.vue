3425f2a8d86b3b526fedafc222131e7972920491
Gabriel Rodriguez <code4kb@gmail.com>
2020-08-06 12:46:06 -0300

    basic project structure

---
+++ src/components/Weather.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h1>Weather</h1>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'Weather',
+    data() {
+        return {
+            weatherData:null,
+            cargando: true,
+            huboerror: false
+        }
+    },
+    mounted(){
+        this.getAll();
+    },
+    methods: {
+        getAll(){
+            this.cargando = true //the loading begin
+            axios
+                .get('weather.json')   
+                .then(response => {
+                    this.weatherData = response.data
+                })
+                .catch(error => {
+                    console.log(error)
+                    this.huboerror = true
+                })
+                .finally(() => this.cargando = false)
+                }
+    }    
+}
+</script>
+
+<style>
+
+</style>
