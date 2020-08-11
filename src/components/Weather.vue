f4089c17ccfd21ef85a3825227dcdb88e07cdf17
Gabriel Rodriguez <code4kb@gmail.com>
2020-08-11 19:02:26 -0300

    modify initial component weather

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,7 +1,8 @@
 <template>
-    <div>
-        <h1>Weather</h1>
-    </div>
+        
+        <h2>Weather App</h2>
+
+    
 </template>
 
 <script>
@@ -22,8 +23,18 @@ export default {
     methods: {
         getAll(){
             this.cargando = true //the loading begin
+            const url = "https://fcc-weather-api.glitch.me/api/current";
+            /*const params = new URLSearchParams();
+            params.append('lat', -38);
+            params.append('lon', -57);*/
             axios
-                .get('weather.json')   
+                .get(url, {
+                    params: {
+                        lat: -38,
+                        lon: -57
+                    }
+                }
+                )
                 .then(response => {
                     this.weatherData = response.data
                 })
@@ -35,8 +46,33 @@ export default {
                 }
     }    
 }
+
 </script>
 
 <style>
+*{
+    margin: 0;
+    padding: 0;
+}
+body{
+    background: -webkit-linear-gradient(to bottom, #49e3ee , #82a2df); 
+    background: linear-gradient(to bottom, #49e3ee , #5085da, #5793f3); 
+    background-repeat: no-repeat;
+    height: 100vh;
+    width: 100vw;
+}
+.small-container {
+	max-width: 420px;
+    text-align: center;
+}
+.card {
+    padding: 30px;
+    background: #fff;
+    border-radius: 20px;
+    margin: 10px;
+}
+.shadow1 { 
+    box-shadow: 0 5px 10px rgba(154,160,185,.05), 0 15px 40px rgba(166,173,201,.2);
+}
 
 </style>
